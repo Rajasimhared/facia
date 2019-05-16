74f01dc6d0129074c9d66166b7ae2b580a262ab8
Rajasimha Reddy <rajasimha5599@gmail.com>
2019-05-16 09:08:46 +0530

    basic overlay on face

---
+++ src/views/ImageInput/index.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { loadModels, getFullFaceDescription } from "../../api/face";
+import testImg from "../../img/rajasimha.jpg";
+
+// Initial State
+const INIT_STATE = {
+  imageURL: testImg,
+  fullDesc: null,
+  detections: null
+};
+
+class ImageInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...INIT_STATE };
+  }
+
+  componentWillMount = async () => {
+    await loadModels();
+    await this.handleImage(this.state.imageURL);
+  };
+
+  handleImage = async (image = this.state.imageURL) => {
+    await getFullFaceDescription(image).then(fullDesc => {
+      if (!!fullDesc) {
+        this.setState({
+          fullDesc,
+          detections: fullDesc.map(fd => fd.detection)
+        });
+      }
+    });
+  };
+
+  handleFileChange = async event => {
+    this.resetState();
+    await this.setState({
+      imageURL: URL.createObjectURL(event.target.files[0]),
+      loading: true
+    });
+    this.handleImage();
+  };
+
+  resetState = () => {
+    this.setState({ ...INIT_STATE });
+  };
+
+  render() {
+    const { imageURL, detections } = this.state;
+
+    let drawBox = null;
+    if (!!detections) {
+      drawBox = detections.map((detection, i) => {
+        let _H = detection.box.height;
+        let _W = detection.box.width;
+        let _X = detection.box._x;
+        let _Y = detection.box._y;
+        return (
+          <div key={i}>
+            <div
+              style={{
+                position: "absolute",
+                border: "solid",
+                borderColor: "blue",
+                height: _H,
+                width: _W,
+                transform: `translate(${_X}px,${_Y}px)`
+              }}
+            />
+          </div>
+        );
+      });
+    }
+
+    return (
+      <div>
+        <input
+          id="myFileUpload"
+          type="file"
+          onChange={this.handleFileChange}
+          accept=".jpg, .jpeg, .png"
+        />
+        <div style={{ position: "relative" }}>
+          <div style={{ position: "absolute" }}>
+            <img src={imageURL} alt="imageURL" />
+          </div>
+          {!!drawBox ? drawBox : null}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(ImageInput);
